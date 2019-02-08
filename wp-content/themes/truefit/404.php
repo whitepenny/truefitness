d42a62d12d5b6b316e4bf0ac58d7ca512133f697
Scott Revell <srevell@whitepenny.com>
2019-02-08 10:40:39 -0500

    Changed 404 Page

--- wp-content/themes/truefit/404.php
+++ wp-content/themes/truefit/404.php
@@ -4,6 +4,29 @@ get_header();
 
 ?>
 <?php get_template_part( 'layouts/page_header', '404' ); ?>
+
+
+
+  <section class="post_detail js-checkpoint" data-checkpoint-animation="fade-up">
+    <div class="fs-row">
+      <div class="fs-cell post_detail_title_cell">
+        
+        <h2 class="post_detail_title">Sorry</h2>
+      </div>
+      <div class="fs-cell fs-lg-7 post_detail_cell">
+        
+        <p>
+            The page you are attempting to access may have been moved or deleted. We apologize for the inconvenience.
+        </p>
+      </div>
+      <div class="fs-cell fs-lg-4 fs-lg-justify-end post_sidebar_cell">
+        <?php get_template_part( 'layouts/sidebar_post' ); ?>
+      </div>
+    </div>
+  </section>
+
+
+
 <?php
 
 get_footer();
